4345e8d7636915daf641ecd5d5aefd79cf4be047
Arcadia <1919463065@qq.com>
2026-05-07 18:58:32 +0800

    chore(master): remove visual quest editor UI and client command entry

--- src/main/java/org/arcadia/arc_quest/client/hud/quest/editor/EditorCommand.java
+++
@@ -1,7 +0,0 @@
-package org.arcadia.arc_quest.client.hud.quest.editor;
-
-public interface EditorCommand {
-    boolean apply();
-    void revert();
-    String description();
-}
--- src/main/java/org/arcadia/arc_quest/client/hud/quest/editor/QuestEditorNodeHitBox.java
+++
@@ -1,7 +0,0 @@
-package org.arcadia.arc_quest.client.hud.quest.editor;
-
-public record QuestEditorNodeHitBox(String nodeId, int x, int y, int width, int height) {
-    public boolean contains(double mouseX, double mouseY) {
-        return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height;
-    }
-}
