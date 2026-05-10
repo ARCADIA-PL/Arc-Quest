9a369d723b2a0ba8909665f23fab0cd574c0f651
Arcadia <1919463065@qq.com>
2026-05-10 17:21:13 +0800

    Add one-click standalone build script

---
+++ arc_quest_editor_modular/build-standalone.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal
+
+set "ROOT_DIR=%~dp0"
+cd /d "%ROOT_DIR%"
+
+echo [1/3] Installing dependencies if needed...
+if not exist "node_modules" (
+  call npm install
+  if errorlevel 1 goto :fail
+)
+
+echo [2/3] Building standalone HTML...
+call npm run build:standalone
+if errorlevel 1 goto :fail
+
+echo [3/3] Renaming output for sharing...
+if exist "dist\arc-quest-editor.html" del /f /q "dist\arc-quest-editor.html"
+if exist "dist\index.html" ren "dist\index.html" "arc-quest-editor.html"
+if errorlevel 1 goto :fail
+
+echo.
+echo Done. Output:
+echo %ROOT_DIR%dist\arc-quest-editor.html
+echo Opening dist folder...
+start "" "%ROOT_DIR%dist"
+goto :eof
+
+:fail
+echo.
+echo Build failed.
+exit /b 1
