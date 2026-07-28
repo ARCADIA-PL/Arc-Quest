8a5fd0258b8dad99d4d4cf9cad4474a9a615f93c
p1nero <1070402601@qq.com>
2026-07-28 23:22:45 +0800

    修复对话选项 Component 样式渲染

--- src/main/java/org/arcadia/arc_quest/dialogue/registry/EpicDialogueTrees.java
+++ src/main/java/org/arcadia/arc_quest/dialogue/registry/EpicDialogueTrees.java
@@ -1,5 +1,6 @@
 package org.arcadia.arc_quest.dialogue.registry;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import org.arcadia.arc_quest.Arc_Quest;
 import org.arcadia.arc_quest.api.ArcQuestAPI;
@@ -360,7 +361,7 @@ public final class EpicDialogueTrees {
                         .say(DialogueText.literal("请点击确认以测试对话中的弹窗"),
                                 "arc_quest:diamond_guide_popup_test_start")
                         .choice("arc_quest:diamond_guide_popup_test_confirm",
-                                DialogueText.literal("确认"),
+                                DialogueText.of(((serverPlayer, entity, iDialogueNpc) -> Component.literal("确认").withStyle(ChatFormatting.AQUA))),
                                 choice -> choice
                                         .unlockGuide("arc_quest:diamond_demo")
                                         .openGuide("arc_quest:diamond_demo", 0, false)
