a8a4ba4c8cf9301e482b5f02bd5a9a6e5ff646eb
Arcadia <1919463065@qq.com>
2026-07-30 15:53:14 +0800

    修复任务历史红点确认状态

--- src/main/java/org/arcadia/arc_quest/client/hud/quest/journal/detail/JournalDetailControls.java
+++ src/main/java/org/arcadia/arc_quest/client/hud/quest/journal/detail/JournalDetailControls.java
@@ -153,6 +153,7 @@ public class JournalDetailControls {
                 return true;
             }
             if (bConfirm && mx >= confirmX && mx <= confirmX + btnW && my >= btnY && my <= btnY + btnH) {
+                screen.acknowledgeQuestChanges(entry.questId());
                 parent.notifyManualPhaseAdvance();
                 ArcQuestNetwork.sendQuestAction(C2SRequestQuestActionPacket.confirmPhaseAdvance(entry.questId(), runtime.getCurrentPendingManualAdvancePhaseId()));
                 screen.playClick();
