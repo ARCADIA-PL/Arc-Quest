913411236f17f489de93905cdce956579764c4e2
p1nero <1070402601@qq.com>
2026-08-20 09:50:40 +0800

    补任务提示，指南改对应按键

--- src/main/java/org/arcadia/arc_quest/data/ArcQuestENLangProvider.java
+++ src/main/java/org/arcadia/arc_quest/data/ArcQuestENLangProvider.java
@@ -60,7 +60,7 @@ public class ArcQuestENLangProvider extends ArcQuestLangProvider {
         add("arc_quest.quest_group.epic_mainline", "Epic Mainline");
 
         add("arc_quest.gui.tracker.open_journal_shortcut", "[%s] Quest Panel");
-        add("arc_quest.gui.tracker.new_quest", "New Quest");
+        add("arc_quest.gui.tracker.new_quest", "New quest available. Press [%s] to view");
         add("arc_quest.gui.tracker.new_guide", "New tutorial available. Press [%s] to view");
         add("arc_quest.guide.splash.status", "NEW TUTORIAL UNLOCKED");
 
